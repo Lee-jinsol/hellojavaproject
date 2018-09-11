d7be08949e2f1c1c39b7615e9127a834c6bace77
jin sol Lee <jinsol9419@naver.com>
2018-09-11 12:58:36 +0900

    Revert "version3 text added"
    This reverts commit 2cbbb99e3554cb32ccdc24a1a2cc69b1fe2eaa2a.

--- main.java
+++ main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(Stirng[] args) {
         System.out.println("Hello Web&App") ;
-        System.out.println("Version2") ;
-        System.out.println("Version3") ;
   }
  }
